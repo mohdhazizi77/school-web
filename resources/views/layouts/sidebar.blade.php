d07484d1971619cd810b23e349e30c427bf4cf37
GG <mohdhaziziak@gmail.com>
2023-05-29 02:36:07 +0800

    second commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="app-sidebar-content" data-scrollbar="true" data-height="100%">
+    <!-- BEGIN menu -->
+    <div class="menu">
+        <div class="menu-header">Navigation</div>
+
+        <div class="menu-item has-sub">
+            <a href="#" class="menu-link">
+							<span class="menu-icon">
+								<i class="far fa-registered"></i>
+							</span>
+                <span class="menu-text">Pendaftaran</span>
+                <span class="menu-caret"><b class="caret"></b></span>
+            </a>
+            <div class="menu-submenu">
+                <div class="menu-item">
+                    <a href="#" class="menu-link">
+                        <span class="menu-text">Sekolah</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
